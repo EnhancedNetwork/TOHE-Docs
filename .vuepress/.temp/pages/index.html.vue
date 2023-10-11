938089802b10602d0a544a96b4ccefaf0d07bf97
Moe <moebstergaming@hotmail.com>
2023-10-10 22:23:48 -0400

    donatebutton

--- .vuepress/.temp/pages/index.html.vue
+++ .vuepress/.temp/pages/index.html.vue
@@ -1,7 +1,7 @@
 <template><div><p align="center">
 <a href="https://discord.gg/tohe" target="_blank"><img src="https://img.shields.io/badge/Discord -%231DA1F2.svg?&style=for-the-badge&logo=discord&logoColor=white&color=5662f6"/></a>&nbsp;
-<a href="https://github.com/0xDrMoe/TownofHost-Enhanced" target="_blank"><img src="https://img.shields.io/badge/Github -%231DA1F2.svg?&style=for-the-badge&logo=github&logoColor=white&color=181717"/></a>&nbsp;
-<a href="https://www.patreon.com/TOHRE" target="_blank"><img src="https://img.shields.io/badge/Patreon-F96854?style=for-the-badge&logo=patreon&logoColor=white"/></a>
+<a href="https://github.com/0xDrMoe/TownofHost-Enhanced" target="_blank"><img src="https://img.shields.io/badge/Github -%231DA1F2.svg?&style=for-the-badge&logo=github&logoColor=white&color=181717"/></a>&nbsp;<br>
+<a href='https://ko-fi.com/TOHEN' target='_blank'><img height='35' style='border:0px;height:57px;' src='@source/images/Donate.png' border='0' alt="" /></a>
 </p>
 <hr>
 <p>To all the amazing players that keep Town of Host: Edited & Town of Host: Re-Edited in their hearts:</p>
@@ -11,7 +11,6 @@ each update that comes</p>
 <p>Please keep in mind that we are still in the Canary (Beta) phase, so not everything is optimized/stable.
 We ask for your patience while we continue to make TOH-<i>Enhanced</i></p>
 <p>Ultimately, we thank you for your patience while we continue to update the mod. TOHE & TOHRE will be remembered with the new name Town of Host: Enhanced!</p>
-<p>Thanks Pyro for your help with the website! - Moe</p>
-</div></template>
+<p>Thanks Pyro for your help with the website! - Moe</p></div></template>
 
 

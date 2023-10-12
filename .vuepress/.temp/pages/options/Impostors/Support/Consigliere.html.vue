4865cdb370ebf5ed243c76f9457cff8c992603cc
Moe <moebstergaming@hotmail.com>
2023-10-12 14:25:20 -0400

    fixbug

---
+++ .vuepress/.temp/pages/options/Impostors/Support/Consigliere.html.vue
@@ -0,0 +1,29 @@
+<template><div><h1 id="👨‍⚖️-consigliere" tabindex="-1"><a class="header-anchor" href="#👨‍⚖️-consigliere" aria-hidden="true">#</a> <font color="red">👨‍⚖️ Consigliere</font> <Badge text="Support" type="tip" vertical="middle"/><Badge text="Added in 2.4.2 Dev 2" type="warning" vertical="middle"/></h1>
+<hr>
+<p>As the Consigiliere, you can reveal the roles of other players using your kill button.<br>
+Single click: Reveal role<br>
+Double click: Kill<br>
+If you run out of reveal uses, your kill button functions normally.</p>
+<ul>
+<li>Max
+<ul>
+<li>Set the Maximum amount of Consiglieres that can be in a match</li>
+</ul>
+</li>
+<li>Kill Cooldown
+<ul>
+<li>Set the Consigiliere’s Kill Cooldown</li>
+</ul>
+</li>
+<li>Maximum Reveals
+<ul>
+<li>Set the Maximum amount of times the Consigliere can reveal a role</li>
+</ul>
+</li>
+</ul>
+<blockquote>
+<p>From: TOHY</p>
+</blockquote>
+</div></template>
+
+

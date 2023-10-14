d25e52480dbf2f78dd4316d9494f8aebf40b6514
Moe <moebstergaming@hotmail.com>
2023-10-13 22:51:01 -0400

    dev4

---
+++ .vuepress/.temp/pages/options/Impostors/Hindering/Anonymous.html.vue
@@ -0,0 +1,33 @@
+<template><div><h1 id="🕵️-anonymous" tabindex="-1"><a class="header-anchor" href="#🕵️-anonymous" aria-hidden="true">#</a> <font color="red">🕵️ Anonymous</font> <Badge text="Hindering" type="tip" vertical="middle"/></h1>
+<hr>
+<p>The Anonymous can Shapeshift into people to make them automatically report any dead body the Anonymous killed, or if no dead body, they will report themself.<br></p>
+<ul>
+<li>Notes:
+<ul>
+<li>Hacking ignores whether a body can be reported</li>
+<li>Hacking Lazy Guy will do nothing
+<br><br></li>
+</ul>
+</li>
+<li>Max
+<ul>
+<li>Set the Maximum amount of Anonymous’ that can be in a match</li>
+</ul>
+</li>
+<li>Kill Cooldown
+<ul>
+<li>Set how long the Anonymous needs to wait to use their Ability/Kill</li>
+</ul>
+</li>
+<li>Ability Use Count
+<ul>
+<li>Set how many times the Anonymous can use his Ability</li>
+</ul>
+</li>
+</ul>
+<blockquote>
+<p>Idea &amp; Coding: <a href="https://github.com/KARPED1EM" target="_blank" rel="noopener noreferrer">KARPED1EM<ExternalLinkIcon/></a></p>
+</blockquote>
+</div></template>
+
+

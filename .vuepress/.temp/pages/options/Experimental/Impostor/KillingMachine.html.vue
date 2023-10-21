6c9d3c2c831b62f4125646a4c0d074f59c3cbfba
Moe <moebstergaming@hotmail.com>
2023-10-20 23:48:16 -0400

    v1.1.0

---
+++ .vuepress/.temp/pages/options/Experimental/Impostor/KillingMachine.html.vue
@@ -0,0 +1,21 @@
+<template><div><h1 id="🔪-killing-machine" tabindex="-1"><a class="header-anchor" href="#🔪-killing-machine" aria-hidden="true">#</a> <font color=red>🔪 <b>Killing Machine</b></font> <Badge text="Impostor" type="tip" vertical="middle"/></h1>
+<hr>
+<p>The Killing Machine has a very short kill cooldown, but cannot vent, has lower vision, cannot sabotage, cannot report, and cannot call emergency meetings. The Killing Machine also has the same vision as Crewmates.</p>
+<ul>
+<li>Max
+<ul>
+<li>Set the Maximum amount of Killing Machines that can be in a match</li>
+</ul>
+</li>
+<li>Kill Cooldown
+<ul>
+<li>Set the Killing Machines Kill Cooldown</li>
+</ul>
+</li>
+</ul>
+<blockquote>
+<p>From: SNR &amp; Coding: <a href="https://github.com/KARPED1EM" target="_blank" rel="noopener noreferrer">KARPED1EM<ExternalLinkIcon/></a></p>
+</blockquote>
+</div></template>
+
+
---
+++ .vuepress/.temp/pages/options/Settings/Neutrals.html.vue
@@ -0,0 +1,93 @@
+<template><div><h1 id="🥷-neutral-roles" tabindex="-1"><a class="header-anchor" href="#🥷-neutral-roles" aria-hidden="true">#</a> <font color="#7f8c8d">🥷 <b>Neutral Roles</b></font></h1>
+<hr>
+<p><b>Play for yourself, and eliminate everyone around you. <font color=gray>Neutrals</font> can win alone, but they can also win with other players. Their victory conditions are different from other players. Each <font color=#7f8c8d>Neutral</font> player has a unique victory condition.</b></p>
+<table>
+<tr>
+<td colspan="2" align="center"><b><u>Role Count</u></b></td>
+</tr>
+<tr>
+<td><font color=#046300><b>Benign</b></font></td>
+<td align="center">11</td>
+</tr>
+<tr>
+<td><font color=#d4af37><b>Chaos</b></font></td>
+<td align="center">11</td>
+</tr>
+<tr>
+<td><font color=#912900><b>Evil</b></font></td>
+<td align="center">7</td>
+</tr>
+<tr>
+<td><font color=#b22222><b>Killing</b></font></td>
+<td align="center">27</td>
+</tr>
+<tr>
+<td><font color=#7c8c8d><b>Total</b></font></td>
+<td align="center">56</td>
+</tr>
+</table>
+<br>
+<h3 id="benign" tabindex="-1"><a class="header-anchor" href="#benign" aria-hidden="true">#</a> <font color=#046300><b>Benign</b></font></h3>
+<hr>
+<p><RouterLink to="/options/Neutrals/Benign/Amnesiac.html">Amnesiac</RouterLink> | <RouterLink to="/options/Neutrals/Benign/Follower.html">Follower</RouterLink> | <RouterLink to="/options/Neutrals/Benign/Hater.html">Hater</RouterLink> | <RouterLink to="/options/Neutrals/Benign/Imitator.html">Imitator</RouterLink> | <RouterLink to="/options/Neutrals/Benign/Lawyer.html">Lawyer</RouterLink> | <RouterLink to="/options/Neutrals/Benign/Maverick.html">Maverick</RouterLink> | <RouterLink to="/options/Neutrals/Benign/Opportunist.html">Opportunist</RouterLink> | <RouterLink to="/options/Neutrals/Benign/Pursuer.html">Pursuer</RouterLink> | <RouterLink to="/options/Neutrals/Benign/Romantic.html">Romantic</RouterLink> | <RouterLink to="/options/Neutrals/Benign/Shaman.html">Shaman</RouterLink> | <RouterLink to="/options/Neutrals/Benign/Witch.html">Witch</RouterLink>
+<br></p>
+<h3 id="chaos" tabindex="-1"><a class="header-anchor" href="#chaos" aria-hidden="true">#</a> <font color=#d4af37><b>Chaos</b></font></h3>
+<hr>
+<p><RouterLink to="/options/Neutrals/Chaos/Collector.html">Collector</RouterLink> | <RouterLink to="/options/Neutrals/Chaos/Cultist.html">Cultist</RouterLink> | <RouterLink to="/options/Neutrals/Chaos/Phantom.html">Phantom</RouterLink> | <RouterLink to="/options/Neutrals/Chaos/Pirate.html">Pirate</RouterLink> | <RouterLink to="/options/Neutrals/Chaos/Provocateur.html">Provocateur</RouterLink> | <RouterLink to="/options/Neutrals/Chaos/Revolutionist.html">Revolutionist</RouterLink> | <RouterLink to="/options/Neutrals/Chaos/SoulCollector.html">Soul Collector</RouterLink> | <RouterLink to="/options/Neutrals/Chaos/Terrorist.html">Terrorist</RouterLink> | <RouterLink to="/options/Neutrals/Chaos/Vector.html">Vector</RouterLink> | <RouterLink to="/options/Neutrals/Chaos/Vulture.html">Vulture</RouterLink> | <RouterLink to="/options/Neutrals/Chaos/Workaholic.html">Workaholic</RouterLink></p>
+<h3 id="evil" tabindex="-1"><a class="header-anchor" href="#evil" aria-hidden="true">#</a> <font color=#912900><b>Evil</b></font></h3>
+<hr>
+<p><RouterLink to="/options/Neutrals/Evil/CursedSoul.html">Cursed Soul</RouterLink> | <RouterLink to="/options/Neutrals/Evil/Doomsayer.html">Doomsayer</RouterLink> | <RouterLink to="/options/Neutrals/Evil/Executioner.html">Executioner</RouterLink> | <RouterLink to="/options/Neutrals/Evil/Innocent.html">Innocent</RouterLink> | <RouterLink to="/options/Neutrals/Evil/Jester.html">Jester</RouterLink> | <RouterLink to="/options/Neutrals/Evil/Masochist.html">Masochist</RouterLink> | <RouterLink to="/options/Neutrals/Evil/Seeker.html">Seeker</RouterLink></p>
+<h3 id="killing" tabindex="-1"><a class="header-anchor" href="#killing" aria-hidden="true">#</a> <font color=red><b>Killing</b></font></h3>
+<hr>
+<p><RouterLink to="/options/Neutrals/Killing/Agitator.html">Agitator</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Arsonist.html">Arsonist</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Bandit.html">Bandit</RouterLink> | <RouterLink to="/options/Neutrals/Killing/BloodKnight.html">Blood Knight</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Demon.html">Demon</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Glitch.html">Glitch</RouterLink> | <RouterLink to="/options/Neutrals/Killing/HexMaster.html">Hex Master</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Huntsman.html">Huntsman</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Infectious.html">Infectious</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Jackal.html">Jackal</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Jinx.html">Jinx</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Juggernaut.html">Juggernaut</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Medusa.html">Medusa</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Necromancer.html">Necromancer</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Occultist.html">Occultist</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Pelican.html">Pelican</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Pickpocket.html">Pickpocket</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Plaguebearer.html">Plaguebearer</RouterLink> | <a href="/options/Neutrals/Killing/Poisoner">Poisoner</a> | <RouterLink to="/options/Neutrals/Killing/PotionMaster.html">Potion Master</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Pyromaniac.html">Pyromaniac</RouterLink> | <RouterLink to="/options/Neutrals/Killing/SerialKiller.html">Serial Killer</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Shroud.html">Shroud</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Stalker.html">Stalker</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Traitor.html">Traitor</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Virus.html">Virus</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Werewolf.html">Werewolf</RouterLink> | <RouterLink to="/options/Neutrals/Killing/Wraith.html">Wraith</RouterLink>
+<br></p>
+<details>
+<summary><font color=#7f8c8d size='4em'><b> Neutral Settings</b></font></summary>
+<br>
+Below are settings to make the game more balanced based on your lobby's style of gameplay:
+<ul>
+<li>Minimum Amount of Non-<font color=#7f8c8d>Neutral</font> Killing roles
+<ul>
+<li>Set the minimal amount of Non-<font color=#7f8c8d>Neutral</font> Killing roles allowed in the round</li>
+</ul>
+</li>
+<li>Maximum Amount of Non-<font color=#7f8c8d>Neutral</font> Killing roles
+<ul>
+<li>Set the max amount of Non-<font color=#7f8c8d>Neutral</font> Killing roles allowed in the round</li>
+</ul>
+</li>
+<li>Minimum Amount of <font color=#7f8c8d>Neutral</font> Killing roles
+<ul>
+<li>Set the minimal amount of <font color=#7f8c8d>Neutral</font> Killing roles allowed in the round</li>
+</ul>
+</li>
+<li>Maximum Amount of <font color=#7f8c8d>Neutral</font> Killing roles
+<ul>
+<li>Set the max amount of Non-<font color=#7f8c8d>Neutral</font> Killing roles allowed in the round</li>
+</ul>
+</li>
+<li>
+<font color=#7f8c8d>Neutrals</font> win together<ul>
+<li>
+<font color=green>ON</font>: Certain <font color=#7f8c8d>Neutral</font> Types will win together<ul>
+<li>If a Killing-<font color=#7f8c8d>Neutral</font> wins, all Killing-<font color=#7f8c8d>Neutrals</font> win. If an Evil-<font color=#7f8c8d>Neutral</font> wins, all Evil-<font color=#7f8c8d>Neutrals</font> win</li>
+</ul>
+</li>
+<li>
+<font color=red>OFF</font>: <font color=#7f8c8d>Neutrals</font> will win on their own team (Ex: Arsonist wins alone)<ul>
+<li>All <font color=#7f8c8d>Neutrals</font> win together
+<ul>
+<li>
+<font color=green>ON</font>: ALL <font color=#7f8c8d>Neutrals</font> win together, even if they are Evil, Killing, or Chaos Neutrals</li>
+<li>
+<font color=red>OFF</font>: Only each <font color=#7f8c8d>Neutral</font> Type will win together</li>
+</ul>
+</li>
+</ul>
+</li>
+</ul>
+</li>
+</ul>
+</details></div></template>
+
+

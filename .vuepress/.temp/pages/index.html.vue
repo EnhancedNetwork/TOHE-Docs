9483b66a11aef56177ce8bb4b53c9890483d174a
Moe <moebstergaming@hotmail.com>
2023-10-28 18:17:23 -0400

    1.2.0fungle

---
+++ .vuepress/.temp/pages/index.html.vue
@@ -0,0 +1,17 @@
+<template><div><p align="center">
+<a href="https://discord.gg/tohe" target="_blank"><img src="https://img.shields.io/badge/Discord -%231DA1F2.svg?&style=for-the-badge&logo=discord&logoColor=white&color=5662f6"/></a>&nbsp;
+<a href="https://github.com/0xDrMoe/TownofHost-Enhanced" target="_blank"><img src="https://img.shields.io/badge/Github -%231DA1F2.svg?&style=for-the-badge&logo=github&logoColor=white&color=181717"/></a>&nbsp;<br>
+<a href='https://ko-fi.com/TOHEN' target='_blank'><img height='35' style='border:0px;height:57px;' src='@source/images/Donate.png' border='0' alt="" /></a>
+</p>
+<hr>
+<p>To all the amazing players that keep Town of Host: Edited & Town of Host: Re-Edited in their hearts:</p>
+<p>We have taken over TOHRE to keep the mod maintained. Moe has taken Project Lead, and will still be maintaining the website, aswell as the Patreon.</p>
+<p>We play Among Us a fair amount, so we will do our best to keep it all updated and viable to play with
+each update that comes</p>
+<p>Please keep in mind that we are still in the Early phase, so not everything is optimized/stable.
+We ask for your patience while we continue to make TOH: <i>Enhanced</i></p>
+<p>Ultimately, we thank you for your patience while we continue to update the mod. TOHE & TOHRE will be remembered with the new name Town of Host: Enhanced!</p>
+<p>Thanks Pyro for your help with the website! - Moe</p><br>
+<p>No Problem! Now uh.. When am I getting paid? - Pyro</p></div></template>
+
+

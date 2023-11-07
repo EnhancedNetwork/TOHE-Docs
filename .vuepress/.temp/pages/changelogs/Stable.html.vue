df1de63c77f902e534ebfaacb45c9627c4df84e0
Moe <32988634+0xDrMoe@users.noreply.github.com>
2023-11-06 19:59:21 -0500

    Delete .vuepress/.temp directory

--- .vuepress/.temp/pages/changelogs/Stable.html.vue
+++
@@ -1,5 +0,0 @@
-<template><div><h1 id="stable-changelogs" tabindex="-1"><a class="header-anchor" href="#stable-changelogs" aria-hidden="true">#</a> Stable Changelogs</h1>
-<font size=3em><b>No Info on this Yet</b></font><br><hr>
-</div></template>
-
-

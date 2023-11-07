507fda50c51b2cbab68b0a4ce5e0ba6760187084
Moe <moebstergaming@hotmail.com>
2023-11-06 20:25:54 -0500

    small-fix

--- .vuepress/.temp/pages/changelogs/Dev.html.vue
+++ .vuepress/.temp/pages/changelogs/Dev.html.vue
@@ -1,87 +1,86 @@
 <template><div><h1 id="dev-changelogs" tabindex="-1"><a class="header-anchor" href="#dev-changelogs" aria-hidden="true">#</a> Dev Changelogs</h1>
 <hr>
 <h2 id="to-access-dev-builds-become-a-sponsor-on-ko-fi-or-contribute" tabindex="-1"><a class="header-anchor" href="#to-access-dev-builds-become-a-sponsor-on-ko-fi-or-contribute" aria-hidden="true">#</a> To access Dev Builds, become a Sponsor on <a href="https://ko-fi.com/tohen" target="_blank" rel="noopener noreferrer">Ko-Fi<ExternalLinkIcon/></a> or <a href="https://github.com/0xDrMoe/TownofHost-Enhanced" target="_blank" rel="noopener noreferrer">Contribute<ExternalLinkIcon/></a></h2>
-<h2 id="v1-2-1-dev-4" tabindex="-1"><a class="header-anchor" href="#v1-2-1-dev-4" aria-hidden="true">#</a> <font size=4em><b>v1.2.1 Dev 4</b></font><br></h2>
+<h2 id="most-recent-v1-2-1-dev-4" tabindex="-1"><a class="header-anchor" href="#most-recent-v1-2-1-dev-4" aria-hidden="true">#</a> <font size=5em><b>Most Recent: v1.2.1 Dev 4</b></font><br></h2>
 <ul>
 <li>By <a href="https://github.com/Tommy-XL" target="_blank" rel="noopener noreferrer"><strong>TommyXL</strong><ExternalLinkIcon/></a><br>
-= <font color=#1376f0><b>BUG FIX</b></font>: Fixed player names for desync impostor during Mushroom Mixup
-= <font color=#1376f0><b>BUG FIX</b></font>: Fixed bug when shapeshift cooldown sets to 0 after Mushroom Mixup (Vanilla Bug)
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix Desync Impostor Reset Cooldown Sabotages (If setting Sabotage Cooldown Control is enabled, desync impostors resets cooldown sabotages when any sabotage is clicked)</li>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fixed player names for desync impostor during Mushroom Mixup <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fixed bug when shapeshift cooldown sets to 0 after Mushroom Mixup (Vanilla Bug) <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix Desync Impostor Reset Cooldown Sabotages (If setting Sabotage Cooldown Control is enabled, desync impostors resets cooldown sabotages when any sabotage is clicked) <br></li>
 </ul>
 <ul>
 <li>By <a href="https://github.com/ryuk2098" target="_blank" rel="noopener noreferrer"><strong>ryuk</strong><ExternalLinkIcon/></a><br>
-- <font color=red><b>REMOVED</b></font>: Removed egoist count as setting from ParityCop
-+ <font color=green><b>NEW</b></font>: Added setting for Egoist to be counted as converted neutral
-+ <font color=green><b>NEW</b></font>: Added setting for converted madmate to kill without repercussions</li>
+- <font color=red><b>REMOVED</b></font>: Removed egoist count as setting from ParityCop <br>
++ <font color=green><b>NEW</b></font>: Added setting for Egoist to be counted as converted neutral <br>
++ <font color=green><b>NEW</b></font>: Added setting for converted madmate to kill without repercussions <br></li>
 </ul>
 <hr>
-<h2 id="v1-2-1-dev-3" tabindex="-1"><a class="header-anchor" href="#v1-2-1-dev-3" aria-hidden="true">#</a> <font size=4em><b>v1.2.1 Dev 3</b></font><br></h2>
+<h2 id="v1-2-1-dev-3" tabindex="-1"><a class="header-anchor" href="#v1-2-1-dev-3" aria-hidden="true">#</a> <font size=5em><b>v1.2.1 Dev 3</b></font><br></h2>
 <ul>
 <li>
 <p>By <a href="https://github.com/NikoCat233" target="_blank" rel="noopener noreferrer"><strong>NikoCat223</strong><ExternalLinkIcon/></a><br>
-= <font color=#F6BE00><b>CHANGE</b></font>: Improved Banning system
-= <font color=#1376f0><b>BUG FIX</b></font>: Fixed converted player win conditions
-= <font color=#1376f0><b>BUG FIX</b></font>: Remove &quot;Incompatible&quot; role assign
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix bug Hurried can be madmate</p>
+= <font color=#F6BE00><b>CHANGE</b></font>: Improved Banning system <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fixed converted player win conditions <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Remove &quot;Incompatible&quot; role assign <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix bug Hurried can be madmate <br></p>
 </li>
 <li>
 <p>By <a href="https://github.com/LezaiYa" target="_blank" rel="noopener noreferrer"><strong>LezaiYa</strong><ExternalLinkIcon/></a><br>
-= <font color=#1376f0><b>BUG FIX</b></font>: Some miscellaneous fixes
-+ <font color=green><b>NEW</b></font>: Basic Crewmate Role: <RouterLink to="/options/Crewmates/Basic/Randomizer.html">Randomizer</RouterLink> (from Night)</p>
+= <font color=#1376f0><b>BUG FIX</b></font>: Some miscellaneous fixes <br>
++ <font color=green><b>NEW</b></font>: Basic Crewmate Role: <RouterLink to="/options/Crewmates/Basic/Randomizer.html">Randomizer</RouterLink> (from Night) <br></p>
 </li>
 <li>
 <p>By <a href="https://github.com/Tommy-XL" target="_blank" rel="noopener noreferrer"><strong>TommyXL</strong><ExternalLinkIcon/></a><br>
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix Doomsayer showing wrong count in message
-= <font color=#F6BE00><b>CHANGE</b></font>: Optimized showing settings in the lobby (Host)
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix Killing Machine Vision
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix /death Command For Host (Hopefully)
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix Killing Machine Can Sabotage
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix Doomsayer showing wrong count in message <br>
+= <font color=#F6BE00><b>CHANGE</b></font>: Optimized showing settings in the lobby (Host) <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix Killing Machine Vision <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix /death Command For Host (Hopefully) <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix Killing Machine Can Sabotage <br>
 + <font color=green><b>NEW</b></font>: Disable Tasks In Fungle</p>
 </li>
 <li>
 <p>By <a href="https://github.com/NetherDragonTw" target="_blank" rel="noopener noreferrer"><strong>NetherDragonTw</strong><ExternalLinkIcon/></a><br>
-= <font color=#F6BE00><b>CHANGE</b></font>: Updated default Template message for Traditional Chinese</p>
+= <font color=#F6BE00><b>CHANGE</b></font>: Updated default Template message for Traditional Chinese <br></p>
 </li>
 <li>
 <p>By <a href="https://github.com/ryuk2098" target="_blank" rel="noopener noreferrer"><strong>ryuk</strong><ExternalLinkIcon/></a><br>
-= <font color=#F6BE00><b>CHANGE</b></font>: Made Vector incompatible with Unlucky
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix Lookout, Inspector and Swapper seeing double id with guesser addon
-= <font color=#F6BE00><b>CHANGE</b></font>: Improved Trickster (Reverie killing Trickster increases cooldown, Always shows as a check mark for witness)
+= <font color=#F6BE00><b>CHANGE</b></font>: Made Vector incompatible with Unlucky <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix Lookout, Inspector and Swapper seeing double id with guesser addon <br>
+= <font color=#F6BE00><b>CHANGE</b></font>: Improved Trickster (Reverie killing Trickster increases cooldown, Always shows as a check mark for witness) <br>
 = <font color=#F6BE00><b>CHANGE</b></font>: Cyber is now incompatible with Doppelganger
-+ <font color=green><b>NEW</b></font>: Neutral Benign Role: <RouterLink to="/options/Neutrals/Benign/Taskinator.html">Taskinator</RouterLink> (idea by Dx)
-+ <font color=green><b>NEW</b></font>: Neutral Benign Role: <RouterLink to="/options/Neutrals/Benign/Pixie.html">Pixie</RouterLink> (idea by Azanthiel)
-+ <font color=green><b>NEW</b></font>: Role colors are now changable trough a new .dat file (User can change role colors by renaming templateRoleColor.dat to RoleColor.dat and adding hex colors in the file; Custom roles can be exported by pressing&quot;F5+X&quot; (same as custom translations))</p>
++ <font color=green><b>NEW</b></font>: Neutral Benign Role: <RouterLink to="/options/Neutrals/Benign/Taskinator.html">Taskinator</RouterLink> (idea by Dx) <br>
++ <font color=green><b>NEW</b></font>: Neutral Benign Role: <RouterLink to="/options/Neutrals/Benign/Pixie.html">Pixie</RouterLink> (idea by Azanthiel) <br>
++ <font color=green><b>NEW</b></font>: Role colors are now changable trough a new .dat file (User can change role colors by renaming templateRoleColor.dat to RoleColor.dat and adding hex colors in the file; Custom roles can be exported by pressing&quot;F5+X&quot; (same as custom translations)) <br></p>
 </li>
 <li>
 <p>By <a href="https://github.com/lars-wu" target="_blank" rel="noopener noreferrer"><strong>lars-wu (papercut)</strong><ExternalLinkIcon/></a><br>
-+ <font color=green><b>NEW</b></font>: Crewmate Support Role: <RouterLink to="/options/Crewmates/Support/Enigma.html">Enigma</RouterLink>
-+ <font color=green><b>NEW</b></font>: impostor Killing role: <RouterLink to="/options/Impostors/Killing/Instigator.html">Instigator</RouterLink></p>
++ <font color=green><b>NEW</b></font>: Crewmate Support Role: <RouterLink to="/options/Crewmates/Support/Enigma.html">Enigma</RouterLink> <br>
++ <font color=green><b>NEW</b></font>: impostor Killing role: <RouterLink to="/options/Impostors/Killing/Instigator.html">Instigator</RouterLink> <br></p>
 </li>
 </ul>
-<hr>
-<h2 id="v1-2-1-dev-2" tabindex="-1"><a class="header-anchor" href="#v1-2-1-dev-2" aria-hidden="true">#</a> <font size=4em><b>v1.2.1 Dev 2</b></font><br></h2>
+<h2 id="v1-2-1-dev-2" tabindex="-1"><a class="header-anchor" href="#v1-2-1-dev-2" aria-hidden="true">#</a> <font size=5em><b>v1.2.1 Dev 2</b></font><br></h2>
 <ul>
 <li>
 <p>By <a href="https://github.com/NikoCat233" target="_blank" rel="noopener noreferrer"><strong>NikoCat233</strong><ExternalLinkIcon/></a> <br>
-= <font color=#1376f0><b>BUG FIX</b></font>: Recode admirer for better performance
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix Admirer knowrole don't work properly
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix Admired Vrctor win condition check
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix Jackal killing Admirer, Sheriff (supposed to recruit) when use only recruit
-= <font color=#F6BE00><b>CHANGE</b></font>: Temporarily prevent Cultist from charming Neutrals.
-= <font color=#F6BE00><b>CHANGE</b></font>: If host is versioncheating, then mod clients wont automatically quit
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix Vulture (Now Vulture cant eat cleaned/stoned body)
-+ <font color=green><b>NEW</b></font>: Harmful Add-on: <RouterLink to="/options/Addons/Harmful/Influenced.html">Influenced</RouterLink>
-+ <font color=green><b>NEW</b></font>: Experimental Add-on: <RouterLink to="/options/Experimental/Addon/OIIAI.html">OIIAI</RouterLink>
-+ <font color=green><b>NEW</b></font>: Harmful Add-on: <RouterLink to="/options/Addons/Harmful/Hurried.html">Hurried</RouterLink> (Added Dev 2, usable Dev 3)</p>
+= <font color=#1376f0><b>BUG FIX</b></font>: Recode admirer for better performance <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix Admirer knowrole don't work properly <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix Admired Vrctor win condition check <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix Jackal killing Admirer, Sheriff (supposed to recruit) when use only recruit <br>
+= <font color=#F6BE00><b>CHANGE</b></font>: Temporarily prevent Cultist from charming Neutrals. <br>
+= <font color=#F6BE00><b>CHANGE</b></font>: If host is versioncheating, then mod clients wont automatically quit <br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix Vulture (Now Vulture cant eat cleaned/stoned body) <br>
++ <font color=green><b>NEW</b></font>: Harmful Add-on: <RouterLink to="/options/Addons/Harmful/Influenced.html">Influenced</RouterLink> <br>
++ <font color=green><b>NEW</b></font>: Experimental Add-on: <RouterLink to="/options/Experimental/Addon/OIIAI.html">OIIAI</RouterLink> <br>
++ <font color=green><b>NEW</b></font>: Harmful Add-on: <RouterLink to="/options/Addons/Harmful/Hurried.html">Hurried</RouterLink> (Added Dev 2, usable Dev 3) <br></p>
 </li>
 <li>
 <p>By <a href="#"><strong>KevOut</strong></a> <br>
-= <font color=#1376f0><b>BUG FIX</b></font>: Fix taskbar always invisible</p>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix taskbar always invisible <br></p>
 </li>
 <li>
 <p>By <a href="https://github.com/Tommy-XL" target="_blank" rel="noopener noreferrer"><strong>TommyXL</strong><ExternalLinkIcon/></a>
-= <font color=#F6BE00><b>CHANGE</b></font>: Improved code base for better performance
-+ <font color=green><b>NEW</b></font>: Camouflage skins (<font color=#ec7578>TommyXL</font> and <font color=#919497>Gurge44</font>)</p>
+= <font color=#F6BE00><b>CHANGE</b></font>: Improved code base for better performance <br>
++ <font color=green><b>NEW</b></font>: Camouflage skins (<font color=#ec7578>TommyXL</font> and <font color=#919497>Gurge44</font>) <br></p>
 </li>
 </ul>
 <hr>

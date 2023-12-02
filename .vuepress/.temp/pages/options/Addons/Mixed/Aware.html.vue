2e2e40f8dca0dbe20ea7005ef5559da45ad72c62
Moe <moebstergaming@hotmail.com>
2023-12-01 19:48:00 -0500

    c2fixes

---
+++ .vuepress/.temp/pages/options/Addons/Mixed/Aware.html.vue
@@ -0,0 +1,53 @@
+<template><div><h1 id="➕-aware" tabindex="-1"><a class="header-anchor" href="#➕-aware" aria-hidden="true">#</a> <font color=#4B0082>➕ <b>Aware</b></font> <Badge text="Mixed" type="tip" vertical="middle"/></h1>
+<hr>
+<p>As Aware, you will be notified in the next meeting if a revealing role interacts with you.</p>
+<ul>
+<li>Max
+<ul>
+<li>Set the Maximum amount of Aware that can be in a match</li>
+</ul>
+</li>
+<li>Spawn Chance
+<ul>
+<li>Set the percentage players have of getting Aware</li>
+</ul>
+</li>
+<li>Impostors can become Aware
+<ul>
+<li>
+<font color=green>ON</font>: Impostors can become Aware</li>
+<li>
+<font color=red>OFF</font>: Impostors cannot become Aware</li>
+</ul>
+</li>
+<li>Crewmates can become Aware
+<ul>
+<li>
+<font color=green>ON</font>: Crewmates can become Aware</li>
+<li>
+<font color=red>OFF</font>: Crewmates cannot become Aware</li>
+</ul>
+</li>
+<li>Neutrals can become Aware
+<ul>
+<li>
+<font color=green>ON</font>: Neutrals can become Aware</li>
+<li>
+<font color=red>OFF</font>: Neutrals cannot become Aware</li>
+</ul>
+</li>
+<li>Knows the role of player
+<ul>
+<li>
+<font color=green>ON</font>: Aware knows the role of the player</li>
+<li>
+<font color=red>OFF</font>: Aware does not know the role of the player</li>
+</ul>
+</li>
+</ul>
+<blockquote>
+<p>From: <a href="#">Dailyhare</a> &amp; Coding: <a href="#">ryuk</a></p>
+</blockquote>
+</div></template>
+
+

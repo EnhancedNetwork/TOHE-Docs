23398e2b473ae20b879d22ab59760f133c1d99f6
Moe <moebstergaming@hotmail.com>
2023-12-13 19:07:06 -0500

    v1.3.1d2h2

---
+++ .vuepress/.temp/pages/Changelogs.html.vue
@@ -0,0 +1,51 @@
+<template><div><h1 id="changelogs" tabindex="-1"><a class="header-anchor" href="#changelogs" aria-hidden="true">#</a> Changelogs</h1>
+<hr>
+<p>(Only the Latest Changes to Mod/Site will be displayed.)</p>
+<hr>
+<h2 id="latest-mod-changelogs-v1-3-1-dev-2-hotfix-2" tabindex="-1"><a class="header-anchor" href="#latest-mod-changelogs-v1-3-1-dev-2-hotfix-2" aria-hidden="true">#</a> <font size=5em><b>Latest Mod Changelogs: v1.3.1 Dev 2 (HOTFIX #2)</b></font><br></h2>
+<ul>
+<li>
+<p>By <a href="https://github.com/NikoCat233" target="_blank" rel="noopener noreferrer"><strong>NikoCat223</strong><ExternalLinkIcon/></a><br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix meeting button broken<br></p>
+</li>
+<li>
+<p>By <a href="https://github.com/ryuk2098" target="_blank" rel="noopener noreferrer"><strong>ryuk</strong><ExternalLinkIcon/></a><br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix Workaholic getting Double Shot<br></p>
+</li>
+</ul>
+<h2 id="v1-3-1-dev-2-hotfix-1" tabindex="-1"><a class="header-anchor" href="#v1-3-1-dev-2-hotfix-1" aria-hidden="true">#</a> <font size=5em><b>v1.3.1 Dev 2 (HOTFIX #1)</b></font><br></h2>
+<p>= I just don't have words.... (proper file attached in this hotfix)<br></p>
+<h2 id="v1-3-1-dev-2" tabindex="-1"><a class="header-anchor" href="#v1-3-1-dev-2" aria-hidden="true">#</a> <font size=5em><b>v1.3.1 Dev 2</b></font><br></h2>
+<ul>
+<li>
+<p>By <a href="https://github.com/NikoCat233" target="_blank" rel="noopener noreferrer"><strong>NikoCat223</strong><ExternalLinkIcon/></a><br>
+= <font color=#e08709><b>IMPROVEMENT</b></font>: Improved Anti first kill Host<br>
+= <font color=#e08709><b>IMPROVEMENT</b></font>: Improved EAC<br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fix ChatCommandPatch<br>
++ <font color=green><b>NEW</b></font>: Experimental Neutral role: <RouterLink to="/options/Experimental/Neutral/Solsticer.html">Solsticer</RouterLink><br></p>
+</li>
+<li>
+<p>By <a href="https://github.com/Pietrodjaowjao" target="_blank" rel="noopener noreferrer"><strong>Pietro</strong><ExternalLinkIcon/></a><br>
++ <font color=green><b>NEW</b></font>: Added new feature: Update Button (Heavy WIP)<br></p>
+</li>
+<li>
+<p>By <a href="https://github.com/ryuk2098" target="_blank" rel="noopener noreferrer"><strong>ryuk</strong><ExternalLinkIcon/></a><br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fixed Taskinator killing alive players<br>
+= <font color=#1376f0><b>BUG FIX</b></font>: Fixed Romantic spamming error<br>
++ <font color=green><b>NEW</b></font>: Crewmate Power role: <RouterLink to="/options/Crewmates/Power/Captain.html">Captain</RouterLink><br></p>
+</li>
+</ul>
+<h4 id="all-changes-commits-can-be-seen-on-github" tabindex="-1"><a class="header-anchor" href="#all-changes-commits-can-be-seen-on-github" aria-hidden="true">#</a> (All Changes/Commits can be seen on <a href="https://github.com/0xDrMoe/TownofHost-Enhanced/commits/main" target="_blank" rel="noopener noreferrer">GitHub<ExternalLinkIcon/></a>)</h4>
+<hr>
+<h2 id="latest-site-changelogs" tabindex="-1"><a class="header-anchor" href="#latest-site-changelogs" aria-hidden="true">#</a> <font size=5em><b>Latest Site Changelogs:</b></font><br></h2>
+<ul>
+<li>By <a href="https://github.com/Pyro0TV" target="_blank" rel="noopener noreferrer"><strong>Pyro</strong><ExternalLinkIcon/></a><br>
+= <font color=#F6BE00><b>CHANGE</b></font>: Added formatting info to <code v-pre>Advanced.md</code> + <code v-pre>TOHE-Data</code> Info<br>
+= <font color=#F6BE00><b>CHANGE</b></font>: Furo's Bio in <code v-pre>AboutUs.md</code> <br>
+= <font color=#F6BE00><b>CHANGE</b></font>: Updated Role Counts, Sidebars, and Tables. <br>
+= Updated to v1.3.1 Dev 2<br>
++ Have a good day!<br></li>
+</ul>
+</div></template>
+
+
---
+++ .vuepress/.temp/pages/options/Addons/Helpful/Cyber.html.vue
@@ -0,0 +1,78 @@
+<template><div><h1 id="📸-cyber" tabindex="-1"><a class="header-anchor" href="#📸-cyber" aria-hidden="true">#</a> <font color=#f46f4e>📸 <b>Cyber</b></font> <Badge text="Helpful" type="tip" vertical="middle"/></h1>
+<hr>
+<p>As the Cyber, you cannot be killed while in a group.<br><br>
+Additionally, your death will be known.</p>
+<ul>
+<li>Max
+<ul>
+<li>Set the Maximum amount of Cybers that can be in a match</li>
+</ul>
+</li>
+<li>Spawn Chance
+<ul>
+<li>Set the percentage players have of getting Cyber</li>
+</ul>
+</li>
+<li>
+<font color=red>Impostors</font> can become Cyber<ul>
+<li>
+<font color=green>ON</font>: <font color=red>Impostors</font> can become this role</li>
+<li>
+<font color=red>OFF</font>: <font color=red>Impostors</font> cannot become this role</li>
+</ul>
+</li>
+<li>
+<font color=#8cffff>Crewmates</font> can become Cyber<ul>
+<li>
+<font color=green>ON</font>: <font color=#8cffff>Crewmates</font> can become this role</li>
+<li>
+<font color=red>OFF</font>: <font color=#8cffff>Crewmates</font> cannot become this role</li>
+</ul>
+</li>
+<li>
+<font color=#7f8c8d>Neutrals</font> can become Cyber<ul>
+<li>
+<font color=green>ON</font>: <font color=#7f8c8d>Neutrals</font> can become this role</li>
+<li>
+<font color=red>OFF</font>: <font color=#7f8c8d>Neutrals</font> cannot become this role</li>
+</ul>
+</li>
+<li>
+<font color=red>Impostors</font> know when the Cyber dies<ul>
+<li>
+<font color=green>ON</font>: <font color=red>Impostors</font> can see the kill flash of when the Cyber dies</li>
+<li>
+<font color=red>OFF</font>: <font color=red>Impostors</font> cannot tell when a Cyber gets killed</li>
+</ul>
+</li>
+<li>
+<font color=#8cffff>Crewmates</font> know when the Cyber dies<ul>
+<li>
+<font color=green>ON</font>: <font color=#8cffff>Crewmates</font> can see the kill flash of when the Cyber dies</li>
+<li>
+<font color=red>OFF</font>: <font color=#8cffff>Crewmates</font> cannot tell when a Cyber gets killed</li>
+</ul>
+</li>
+<li>
+<font color=#7f8c8d>Neutrals</font> know when the Cyber dies<ul>
+<li>
+<font color=green>ON</font>: <font color=#7f8c8d>Neutrals</font> can see the kill flash of when the Cyber dies</li>
+<li>
+<font color=red>OFF</font>: <font color=#7f8c8d>Neutrals</font> cannot tell when a Cyber gets killed.</li>
+</ul>
+</li>
+<li>Everyone can see Cyber:
+<ul>
+<li>
+<font color=green>ON</font>: a <font color=#f46f4e>orange</font> star will appear next to the Cyber’s name</li>
+<li>
+<font color=red>OFF</font>: the Cyber remains secret</li>
+</ul>
+</li>
+</ul>
+<blockquote>
+<p>From: Idea &amp; Coding: <a href="https://github.com/Loonie-Toons/" target="_blank" rel="noopener noreferrer">LoonieToons<ExternalLinkIcon/></a></p>
+</blockquote>
+</div></template>
+
+

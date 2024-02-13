0ab4b3545faf5fc9760c5d5b72756c3739df20b9
Moe <moebstergaming@hotmail.com>
2024-02-13 15:10:49 -0500

    fix

---
+++ .vuepress/.temp/pages/zh-CN/GetStarted.html.vue
@@ -0,0 +1,73 @@
+<template><div><h1 id="installation" tabindex="-1"><a class="header-anchor" href="#installation" aria-hidden="true">#</a> INSTALLATION</h1>
+<h2 id="distribution-policy" tabindex="-1"><a class="header-anchor" href="#distribution-policy" aria-hidden="true">#</a> Distribution Policy</h2>
+<p><b>Dev and Canary Builds are not for distribution. If you are found to be distributing these builds, you will be removed from the respective role and your access to the builds will be revoked. <br>
+If you are a content creator, you may use these builds in your videos, but you may not provide a download link to your viewers. If you are found to be distributing these builds, you will be removed from the respective role and your access to the builds will be revoked.<br><br>
+This policy is in place to keep the integrity of our Sponsor, Contributor, Tester, Nitro Booster, and Translator roles. If you are interested in becoming one of these roles, please see the below sections for more information.</b></p>
+<h2 id="manual-installation" tabindex="-1"><a class="header-anchor" href="#manual-installation" aria-hidden="true">#</a> MANUAL INSTALLATION</h2>
+<p>Installation requires that you own a PC version of Among Us on Steam, Epic, or Itch.</p>
+<ol>
+<li>
+<p>Download the latest full Release of TOHE in #mod-release or <a href="https://github.com/0xDrMoe/TownofHost-Enhanced/releases" target="_blank" rel="noopener noreferrer">here<ExternalLinkIcon/></a>. You only need the main zip such as &quot;TOHE.v1.0.0.zip&quot;, not the source code files.</p>
+</li>
+<li>
+<p>Once the download is complete, unzip it into the location of your choice. Example: C:\TOHE</p>
+</li>
+<li>
+<p>Locate your AmongUs install. For example, if you own the game on Steam, you can find this by right clicking on the game in your library, selecting &quot;Manage&quot;, and finally selecting &quot;Browse local files.&quot;
+<img src="@source/images/SteamGetFolder.png" alt="image"></p>
+</li>
+<li>
+<p>Select all of the files in your AmongUs installation and copy them to the folder that now has the files from the TOHE release. The folder should now look something like this:
+<img src="@source/images/ResultFolder.png" alt="image"></p>
+</li>
+<li>
+<p>Within the TOHE folder, run Among Us.exe. The first time running it may take some time while the mod sets up its files. If you see a black screen, just wait a couple minutes. Once it finishes loading, you should get a popup with TOHE news. If you do not see anything about TOHE, double check that your files are all in the same folder.</p>
+</li>
+<li>
+<p>(Optional) Right click your Among Us.exe and click 'Create shortcut'. Move your new shortcut to wherever you can easily access it, such as your Desktop!</p>
+</li>
+</ol>
+<h4 id="you-have-now-installed-town-of-host-enhanced-all-that-remains-is-to-start-up-a-private-lobby-and-customize-your-settings" tabindex="-1"><a class="header-anchor" href="#you-have-now-installed-town-of-host-enhanced-all-that-remains-is-to-start-up-a-private-lobby-and-customize-your-settings" aria-hidden="true">#</a> You have now installed Town of Host: Enhanced! All that remains is to start up a private lobby and customize your settings.</h4>
+<h2 id="new-releases" tabindex="-1"><a class="header-anchor" href="#new-releases" aria-hidden="true">#</a> New Releases</h2>
+<p>Keep an eye on the <a href="https://github.com/0xDrMoe/TownofHost-Enhanced/releases" target="_blank" rel="noopener noreferrer">releases page<ExternalLinkIcon/></a> for new releases. If a new version is released, all you'll usually need to do is repeat steps 1-2 above, unzipping into the same folder where you already have it installed and letting it overwrite. If it says it requires a newer version of Among Us, repeat steps 3-5 as well after making sure your base game is up to date.</p>
+<h2 id="installing-dev-builds-canary-builds" tabindex="-1"><a class="header-anchor" href="#installing-dev-builds-canary-builds" aria-hidden="true">#</a> Installing Dev Builds/Canary Builds</h2>
+<ol>
+<li>
+<p>Download the latest dll in #dev-builds or #canary-builds.</p>
+</li>
+<li>
+<p>Once the download is complete, move the dll into your Among Us\BepInEx\plugins folder. (&quot;Among Us&quot; being your modded Among Us folder.)</p>
+</li>
+<li>
+<p>Within the TOHE folder, run Among Us.exe. Once it finishes loading, you should get a popup with TOHE news. If you do not see anything about TOHE, double check that your files are all in the same folder.</p>
+</li>
+<li>
+<p>(Optional) Right click your Among Us.exe and click 'Create shortcut'. Move your new shortcut to wherever you can easily access it, such as your Desktop!</p>
+</li>
+</ol>
+<h4 id="you-have-successfully-installed-a-dev-build-canary-build" tabindex="-1"><a class="header-anchor" href="#you-have-successfully-installed-a-dev-build-canary-build" aria-hidden="true">#</a> You have successfully installed a Dev Build/Canary Build!</h4>
+<h2 id="how-to-gain-dev-builds-access" tabindex="-1"><a class="header-anchor" href="#how-to-gain-dev-builds-access" aria-hidden="true">#</a> How to gain Dev Builds Access<br></h2>
+<ul>
+<li>Accessible by Contributors, Testers, and Sponsors that are at least Insider tier.<br>
+<code v-pre>NOT FOR DISTRIBUTION</code></li>
+</ul>
+<ul>
+<li>To become a <b>Contributor</b>, you need to help with coding in roles/addons/gamemodes/etc related to the mod</li>
+<li>To become a <b>Tester</b>, you must be an active content creator with your <b>TOHE videos</b> having 600+ views on average</li>
+<li>To become a <b>Sponsor</b>, you need to purchase a tier on our <a href="https://ko-fi.com/TOHEN" target="_blank" rel="noopener noreferrer">Ko-fi<ExternalLinkIcon/></a> (For Dev Builds access specifically, you need to purchase at least Insider tier for $5.00)</li>
+</ul>
+<p>Once you have been accepted into one of the above roles, you will be able to access the #dev-builds channel in the <a href="https://discord.gg/tohe" target="_blank" rel="noopener noreferrer">Town of Host: Enhanced Discord<ExternalLinkIcon/></a></p>
+<h2 id="how-to-gain-canary-builds-access" tabindex="-1"><a class="header-anchor" href="#how-to-gain-canary-builds-access" aria-hidden="true">#</a> How to gain Canary Builds Access<br></h2>
+<ul>
+<li>Accessible by Translators, Nitro Boosters, and Sponsors that are at least Initiate tier.<br>
+<code v-pre>NOT FOR DISTRIBUTION</code></li>
+</ul>
+<ul>
+<li>To become a <b>Nitro Booster</b>, you must boost the <a href="https://discord.gg/tohe" target="_blank" rel="noopener noreferrer">Town of Host: Enhanced Discord<ExternalLinkIcon/></a> server</li>
+<li>To become a <b>Translator</b>, you need to help with translating the mod into other languages. You can find more information about this in the <a href="https://discord.gg/tohe" target="_blank" rel="noopener noreferrer">Town of Host: Enhanced Discord<ExternalLinkIcon/></a></li>
+<li>To become a <b>Sponsor</b>, you need to purchase a tier on our <a href="https://ko-fi.com/TOHEN" target="_blank" rel="noopener noreferrer">Ko-fi<ExternalLinkIcon/></a> (For Canary Builds access specifically, you need to purchase at least Initiate tier (lowest tier) for $3.00)</li>
+</ul>
+<p>Once you have been accepted into one of the above roles, you will be able to access the #canary-builds channel in the <a href="https://discord.gg/tohe" target="_blank" rel="noopener noreferrer">Town of Host: Enhanced Discord<ExternalLinkIcon/></a>.</p>
+</div></template>
+
+

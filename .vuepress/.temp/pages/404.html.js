1f417ac8b90e65c23ab13d05560612a5e6c176fc
Pyro <btriche.sots@gmail.com>
2025-04-11 00:12:04 -0400

    delete the fat temp and dist files again cuz ts stupid

--- .vuepress/.temp/pages/404.html.js
+++
@@ -1,3 +0,0 @@
-import comp from "C:/Users/btric/OneDrive/Documents/Robert Important Files/toheDEV/docs/.vuepress/.temp/pages/404.html.vue"
-const data = JSON.parse("{\"path\":\"/404.html\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{\"layout\":\"NotFound\"},\"headers\":[],\"git\":{},\"filePathRelative\":null}")
-export { comp, data }
